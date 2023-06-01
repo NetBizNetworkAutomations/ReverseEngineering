0f2d9f02244450205684d60b586bfa3b5a1628fe
ko-yamazaki <yamazaki_koutabb@yahoo.co.jp>
2023-06-01 16:11:57 +0900

    load_init_ssh.py修正

--- 10_network_automation_training/10_basic/Chapter2/lab/load_init_ssh.py
+++ 10_network_automation_training/10_basic/Chapter2/lab/load_init_ssh.py
@@ -20,21 +20,23 @@ for host in hosts:
 	net_connect.enable()
 	
 	print(host['hostname'] + 'で初期化用の設定をstartup-configへコピーします')
-
+    
 	cmd = 'copy flash:python_init.cfg startup-config'
-    net_connect.send_command_timing(cmd, strip_prompt=False, strip_command=False)
-    net_connect.send_command_timing('\n', delay_factor=5, strip_prompt=False, strip_command=False)
+	net_connect.send_command_timing(cmd, strip_prompt=False, strip_command=False)
+	net_connect.send_command_timing('\n', delay_factor = 5, strip_prompt=False, strip_command=False)
 
-    print(host['hostname'] + 'を再起動します・・・')
-    cmd = 'reload'
-    result = net_connect.send_command_timing(cmd, strip_prompt=False, strip_command=False)
+	print(host['hostname'] + 'を再起動します・・・')
+	cmd = 'reload'
+	result = net_connect.send_command_timing(cmd, strip_prompt=False, strip_command=False)
 
-    if 'confirm' in result:
-        net_connect.send_command_timing('\n', strip_prompt=False, strip_command=False)
-    elif 'System' in result:
-        net_connect.send_command_timing('no', strip_prompt=False, strip_command=False)
-        net_connect.send_command_timing('\n', strip_prompt=False, strip_command=False)
-    else:
-        print('再起動に失敗しました')
+	if 'confirm' in result:
+		net_connect.send_command_timing('\n', strip_prompt=False, strip_command=False)
+	elif 'System' in result:
+		net_connect.send_command_timing('no', strip_prompt=False, strip_command=False)
+		net_connect.send_command_timing('\n', strip_prompt=False, strip_command=False)
+	else:
+		print('再起動に失敗しました')
+	
+	print('-'*50)
 
-    print('-' * 50)
+print('スクリプトが終了しました。\nデバイスが起動するまでしばらくお待ちください')
