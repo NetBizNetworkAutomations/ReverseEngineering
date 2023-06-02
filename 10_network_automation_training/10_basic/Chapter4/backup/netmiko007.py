31196940f32c2a975f3d3ca0a45700c5978c718f
ko-yamazaki <yamazaki_koutabb@yahoo.co.jp>
2023-06-02 20:27:44 +0900

    netmiko007.py修正

--- 10_network_automation_training/10_basic/Chapter4/backup/netmiko007.py
+++ 10_network_automation_training/10_basic/Chapter4/backup/netmiko007.py
@@ -11,7 +11,7 @@ net_connect = ConnectHandler(**cisco)
 
 net_connect.enable()
 
-cmd = ['int gi0/1', 'description “Connect To PC”']
+cmd = ['int gi0/1', 'description \“Connect To PC\”']
 net_connect.send_config_set(cmd)
 
 net_connect.disconnect()
