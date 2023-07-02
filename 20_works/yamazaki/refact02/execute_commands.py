ab67bb6525aa30639fb81e62607f10b92698a27d
ko-yamazaki <yamazaki_koutabb@yahoo.co.jp>
2023-07-02 21:56:09 +0900

    show ip route追加

--- 20_works/yamazaki/refact02/execute_commands.py
+++ 20_works/yamazaki/refact02/execute_commands.py
@@ -22,18 +22,31 @@ def execute_show_interfaces(connection, cmd, ws, row):
     
     return ws, row
 
-def execute_show_version(connection, cmd, ws, row):
-    # ここに 'another command' の実行と結果の処理を書く
-    # 以下はダミーのコード
-    # output = connection.send_command(cmd, use_textfsm=True)
+def execute_show_ip_route(connection, cmd, ws, row):
+    # コマンド実行結果取得    
+    output = connection.send_command(cmd, use_textfsm=True)
     # outputの処理を書く
+    
+    # 取得したデータをExcelに出力する
+    for x in output:
+        ws.cell(row, 2).value = x['protocol']
+        ws.cell(row, 3).value = x['network']
+        
+        mask = IPNetwork('0.0.0.0/' + x['mask'])
+        
+        ws.cell(row, 4).value = str(mask.netmask)
+        ws.cell(row, 5).value = x['nexthop_ip'] or '-'
+        ws.cell(row, 6).value = x['nexthop_if'] or '-'
+        
+        row += 1
+
     return ws, row
 
 def execute_commands(connection, commands, ws):
     for cmd in commands:
         if cmd == 'show interfaces':
             ws, row = execute_show_interfaces(connection, cmd, ws, 17)
-        elif cmd == 'show version':
-            ws, row = execute_show_version(connection, cmd, ws, 10)
+        elif cmd == 'show ip route':
+            ws, row = execute_show_ip_route(connection, cmd, ws, 49)
         # 他のコマンドに対応する場合は、elif節を追加する
     return ws
