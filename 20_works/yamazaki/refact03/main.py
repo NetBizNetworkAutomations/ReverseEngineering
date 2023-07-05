bb44864aa44749fb46e75e56ca5306569b7204fc
ko-yamazaki <92278670+ko-yamazaki@users.noreply.github.com>
2023-07-05 08:15:11 +0000

    main処理の修正

--- 20_works/yamazaki/refact03/main.py
+++ 20_works/yamazaki/refact03/main.py
@@ -1,46 +1,7 @@
-import csv
-from netmiko import ConnectHandler
-from network_manager import create_device
-from excel_handler import open_workbook, save_excel
-from cisco_ios.show_interfaces import execute_show_interfaces
-from cisco_ios.show_ip_route import execute_show_ip_route
+from reverse_operations import run_reverse_operations
 
-# ホスト情報を読み込む
-with open('hosts.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    hosts = list(reader)
+def main():
+    run_reverse_operations()
 
-# 各ホストに接続し、情報を取得する
-for host in hosts:
-
-    # 接続情報生成
-    device = create_device(host)
-
-    # ホストへ接続
-    connection = ConnectHandler(**device)
-
-    # enableモードへ移行
-    connection.enable()
-
-    # Excelワークブックを開く
-    filename = host['filename']
-    wb = open_workbook(filename)
-
-    # コマンド実行結果取得とExcelへの出力
-    commands = ['show interfaces', 'show ip route']
-    for cmd in commands:
-        if cmd == 'show interfaces':
-            # 必要により個別のワークシートを指定
-            ws_interfaces = wb['Tokyo']
-            ws_interfaces, row_interfaces = execute_show_interfaces(connection, cmd, ws_interfaces, 17)
-        elif cmd == 'show ip route':
-            # 必要により個別のワークシートを指定
-            ws_routes = wb['Tokyo']
-            ws_routes, row_routes = execute_show_ip_route(connection, cmd, ws_routes, 29)
-
-    # エクセルファイルの保存
-    save_excel(wb, filename)
-
-    # ホスト接続の解除
-    connection.disconnect()
-
+if __name__ == "__main__":
+    main()
