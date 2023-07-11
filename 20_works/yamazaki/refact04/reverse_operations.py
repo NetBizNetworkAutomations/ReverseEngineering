0ed2311fa60095ce31c5f594d0c3b1c209711e2c
ko-yamazaki <92278670+ko-yamazaki@users.noreply.github.com>
2023-07-11 01:55:46 +0000

    CSVファイルの指定をiniファイルへ外出し

--- 20_works/yamazaki/refact04/reverse_operations.py
+++ 20_works/yamazaki/refact04/reverse_operations.py
@@ -1,11 +1,16 @@
+import configparser
 from netmiko import ConnectHandler
 from network_manager import create_device, load_hosts
 from excel_handler import open_workbook, save_excel
 from command_executor import execute_commands
 
 def run_reverse_operations():
+    # 設定ファイルを読み込む
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
     # ホスト情報を読み込む
-    hosts = load_hosts('hosts.csv')
+    hosts = load_hosts(config['DEFAULT']['HostsCSV'])
 
     # 各ホストに接続し、情報を取得する
     for host in hosts:
@@ -24,7 +29,7 @@ def run_reverse_operations():
         wb = open_workbook(filename)
 
         # コマンド実行結果取得とExcelへの出力
-        execute_commands(connection, wb, 'commands.csv')
+        execute_commands(connection, wb, config['DEFAULT']['CommandsCSV'])
 
         # エクセルファイルの保存
         save_excel(wb, filename)
